fa2c882504bbb7a8fd12cc1300554bc086440f52
givemin5 <givemin5@gmail.com>
2017-01-16 23:53:38 +0800

    😈 第一集買了一本_第二集也買了一本_價格應為190

--- PotterShoppingCart.Tests/UnitTest1.cs
+++ PotterShoppingCart.Tests/UnitTest1.cs
@@ -24,6 +24,22 @@ namespace PotterShoppingCart.Tests
             var expected = 100;
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Test_第一集買了一本_第二集也買了一本_價格應為190() {
+            //Arrange
+            var cart = new ShoppingCart();
+            var book1 = new Book { Title = "哈利波特第一集", Price = 100, Quantity = 1 };
+            var book2 = new Book { Title = "哈利波特第二集", Price = 100, Quantity = 1 };
+            //Act
+            cart.Add(book1);
+            cart.Add(book2);
+            int actual = cart.Checkout();
+
+            //Assert
+            var expected = 190;
+            Assert.AreEqual(expected, actual);
+        }
     }
 
     internal class Book
